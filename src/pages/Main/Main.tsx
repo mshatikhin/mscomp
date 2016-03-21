baf8b5ce06d7169834f80824a1cb8cc441abc961
Mikhail Shatikhin <mshatikhin@gmail.com>
2016-03-21 13:04:20 +0500

    index

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -10,7 +10,7 @@ class Main extends React.Component<any, IMainState> {
         this.state = {}
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.getPicture("87182509@N05");
     }
 
@@ -20,33 +20,31 @@ class Main extends React.Component<any, IMainState> {
         var url_to_a_photo_head = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + apiKey + "&photo_id=";
         var url_to_a_photo_tail = "&format=json&jsoncallback=?";
 
-        // get an array of random photos
-        var options = {
+        $.getJSON("https://api.flickr.com/services/rest/", {
             method: 'flickr.people.getPublicPhotos',
             api_key: apiKey,
             user_id: the_user_id,
             format: 'json',
             nojsoncallback: 1,
             per_page: 10 // you can increase this to get a bigger array
-        };
-        var success = function (data: any) {
+        }, function (data: any) {
             // if everything went good
             if (data.stat == 'ok') {
                 // get a random id from the array
-                var photoId = data.photos.photo[Math.floor(Math.random() * data.photos.photo.length)];
+                var photo = data.photos.photo[Math.floor(Math.random() * data.photos.photo.length)];
 
                 // now call the flickr API and get the picture with a nice size
                 var options2 = {
                     method: 'flickr.photos.getSizes',
                     api_key: apiKey,
-                    photo_id: photoId,
+                    photo_id: photo.id,
                     format: 'json',
                     nojsoncallback: 1
                 };
                 $.getJSON("https://api.flickr.com/services/rest/", options2, function (response: any) {
                         if (response.stat == 'ok') {
-                            var the_url = response.sizes.size[5].source;
-                            $("body").css({'background': the_url});
+                            var the_url = response.sizes.size[8].source;
+                            $("."+styles.main).css({background: 'url(' + the_url + ') no-repeat 50% 50%', backgroundSize: 'cover;'});
                         }
                         else {
                             console.log(" The request to get the picture was not good :\ ")
@@ -57,14 +55,12 @@ class Main extends React.Component<any, IMainState> {
             else {
                 console.log(" The request to get the array was not good :( ");
             }
-        };
-        $.getJSON("https://api.flickr.com/services/rest/", options, success);
+        });
     };
 
     render() {
         return (
             <div className={styles.main}>
-                Website loading please wait...
             </div>
         );
     }
