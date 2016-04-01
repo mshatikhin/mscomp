9935464298253e32166e5ae80581a39712fcb899
Mikhail Shatikhin <mshatikhin@gmail.com>
2016-04-01 16:22:56 +0500

    flickr

--- src/components/Api/FlickrClient.ts
+++ src/components/Api/FlickrClient.ts
@@ -1,8 +1,70 @@
 /**
  * Created by mshat on 28.03.2016.
  */
+
+interface IFlickrPhoto {
+    id: string;
+    secret: string;
+    server: string;
+    farm: number
+    title: string;
+}
+
+interface IFlickrPhotoSet {
+    id: string;
+    title: string;
+    description: string;
+    photos: string;
+}
+
+interface IFlickrPhotosets {
+    photoset: IFlickrPhotoSet[];
+}
+
+interface IFlickrResponse {
+    stat: string;
+    photosets: IFlickrPhotosets;
+}
+
 class FlickrClient {
-    
+    constructor() {
+    }
+
+    getPhotos = (userId: string, apiKey: string, cb: (data: any)=>void)=> {
+        $.getJSON("https://api.flickr.com/services/rest/",
+            {
+                method: 'flickr.photosets.getList',
+                api_key: apiKey,
+                user_id: userId,
+                format: 'json',
+                nojsoncallback: 1,
+                per_page: 10
+            }, (response: IFlickrResponse)=> {
+                if (response.stat === "ok") {
+                    response.photosets.photoset.forEach((p: IFlickrPhotoSet)=> {
+
+                    });
+                    $.getJSON("https://api.flickr.com/services/rest/",
+                        {
+                            method: 'flickr.photosets.getPhotos',
+                            api_key: apiKey,
+                            photoset_id: response.photosets.photoset[0].id,
+                            user_id: userId,
+                            format: 'json',
+                            nojsoncallback: 1,
+                            per_page: 10
+                        }, (response: any)=> {
+                            console.log(response);
+                            if (response.stat === "ok") {
+                                const photos = response.photoset.photo.map((photo: IFlickrPhoto)=> {
+                                    return `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`
+                                })
+                                cb(photos)
+                            }
+                        });
+                }
+            });
+    };
 }
 
 export default FlickrClient;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,96 +1,44 @@
 ﻿const styles: any = require("./Main.css");
 import { Paper } from "material-ui";
+import FlickrClient from "../../components/Api/FlickrClient";
 
 interface IMainState {
-    url: string;
+    photos: string[];
 }
 var interval: any = null;
 class Main extends React.Component<any, IMainState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            url: ""
+            photos: []
         }
     }
 
-    setPicture = (url: string)=> {
-        this.setState({
-            url: url
-        })
+    componentWillMount() {
+        this.getPictures()
     }
 
-    componentDidMount() {
-        this.getPicture("124274905@N03");
-        interval = setInterval(()=> {
-            this.getPicture("124274905@N03");
-        }, 10000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(interval);
-    }
-
-    getPicture = (the_user_id: string)=> {
-        var apiKey = "1173960c94df6700f0b57dccc50f0925"; // replace this with your API key
-        // $.getJSON("https://api.flickr.com/services/rest/",
-        //     {
-        //         method: 'flickr.photosets.getList',
-        //         api_key: apiKey,
-        //         user_id: the_user_id,
-        //         format: 'json',
-        //         nojsoncallback: 1,
-        //         per_page: 10 // you can increase this to get a bigger array
-        //     },(response: any)=>{ console.log(response)});
-
-        $.getJSON("https://api.flickr.com/services/rest/",
-            {
-                method: 'flickr.people.getPublicPhotos',
-                api_key: apiKey,
-                user_id: the_user_id,
-                format: 'json',
-                nojsoncallback: 1,
-                per_page: 100 // you can increase this to get a bigger array
-            }, (data: any)=> {
-                if (data.stat == 'ok') {
-                    // get a random id from the array
-                    var photo = data.photos.photo[Math.floor(Math.random() * data.photos.photo.length)];
-
-                    // now call the flickr API and get the picture with a nice size
-                    var options2 = {
-                        method: 'flickr.photos.getSizes',
-                        api_key: apiKey,
-                        photo_id: photo.id,
-                        format: 'json',
-                        nojsoncallback: 1
-                    };
-                    $.getJSON("https://api.flickr.com/services/rest/", options2, (response: any)=> {
-                            if (response.stat == 'ok') {
-                                var the_url = response.sizes.size[8].source;
-                                this.setPicture(the_url);
-                            }
-                            else {
-                                console.log(" The request to get the picture was not good :\ ")
-                            }
-                        }
-                    );
-                }
-                else {
-                    console.log(" The request to get the array was not good :( ");
-                }
+    getPictures = ()=> {
+        var flickrClient = new FlickrClient();
+        flickrClient.getPhotos("124274905@N03", "1173960c94df6700f0b57dccc50f0925", (photos)=> {
+            this.setState({
+                photos: photos
             });
+        })
     };
 
     render() {
         return (
             <div className={styles.main}>
-                {this.state.url !== "" && <Paper zDepth={1}>
-                    <a href="https://www.flickr.com/photos/124274905@N03/"
-                       target="blank">
-                        <img className={styles.mainImage}
-                             src={this.state.url}
-                             height="600"/>
-                    </a>
-                </Paper>}
+                {this.state.photos.map((url)=> {
+                    return  <Paper zDepth={1}>
+                        <a href="https://www.flickr.com/photos/124274905@N03/"
+                           target="blank">
+                            <img className={styles.mainImage}
+                                 src={url}/>
+                        </a>
+                    </Paper>
+                })}
             </div>
         );
     }
