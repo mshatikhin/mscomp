b2ea74e793c3548218405bfedf2d83cdeb61fc95
mshatikhin <mshatikhin@gmail.com>
2016-10-10 09:45:22 +0500

    refactor state

--- src/redux/reducers/blogReducer.js
+++ src/redux/reducers/blogReducer.js
@@ -6,14 +6,15 @@ type IState = {
     posts: any[];
 }
 
-const initialState = {
+const initialState: IState = {
     posts: []
 };
 
 export default function (state: IState = initialState, action: any) {
     switch (action.type) {
         case UPDATE_BLOG:
-            return Object.assign({}, state, action.blog);
+            state.posts = action.blog.posts;
+            return Object.assign({}, state);
         default:
             return state;
     }
