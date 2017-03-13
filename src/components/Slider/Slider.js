cfeb3655666c0e28c74a9d628af7cddc31b706d3
mshatikhin <mshatikhin@gmail.com>
2017-03-13 22:15:00 +0500

    Сделал слайдер

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -7,21 +7,38 @@ import styles from "./Slider.css";
 
 type Props = {
     initialSlide: number;
+    children: any[];
 }
 
 type State = {
     selectedSlide: number;
+    slideWidth: number;
+    autoplay: boolean;
 }
 
 class Slider extends Component {
     props: Props;
     state: State;
+    sliderElement: Element;
+    autoplayInterval: any;
 
-    constructor(props) {
+    constructor(props: Props) {
         super(props);
         this.state = {
-            selectedSlide: 0
+            selectedSlide: 0,
+            slideWidth: 0,
+            autoplay: true
         }
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.setSliderSize);
+        this.setSliderSize();
+        this.setAutoPlay();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.setSliderSize);
     }
 
     prev = () => {
@@ -38,11 +55,39 @@ class Slider extends Component {
         })
     };
 
+    getWidthSlider = (): number => {
+        const slider = this.sliderElement;
+        if (slider) {
+            let computedStyle = getComputedStyle(slider);
+            let width = computedStyle.width;
+            return parseInt(width);
+        }
+        return 0;
+    };
+
+    setSliderSize = () => {
+        this.setState({
+            slideWidth: this.getWidthSlider()
+        })
+    };
+
+    setAutoPlay = (stop: boolean = false) => {
+        if (stop) {
+            clearInterval(this.autoplayInterval);
+            this.autoplayInterval = null
+        } else if (this.autoplayInterval == null) {
+            this.autoplayInterval = setInterval(this.next, 3000);
+        }
+    };
+
     render() {
-        const slideWidth = 1080; //todo: width of slider
-        const marginLeft = this.state.selectedSlide * -slideWidth;
+        const marginLeft = this.state.selectedSlide * -this.state.slideWidth;
         return (
-            <div className={styles.root}>
+            <div className={styles.root}
+                 ref={el => this.sliderElement = el}
+                 onMouseDown={()=> this.setAutoPlay(true)}
+                 onClick={()=> this.setAutoPlay(false)}
+            >
                 <div className={styles.slides} style={{ marginLeft: marginLeft }}>
                     {React.Children.map(this.props.children, (child) => <div
                         className={styles.imageWrap}>{child}</div>)}
